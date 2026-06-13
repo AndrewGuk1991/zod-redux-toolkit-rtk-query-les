71d40ade3a67c4e0a8c83700a97589bc80abb100
andrew_guk <gukandrey91@gmail.com>
2026-06-13 19:45:48 +0300

    add refactor Zod validation

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,7 +1,17 @@
-export type FieldError = {
-  error: string
-  field: string
-}
+import { z } from "zod"
+import { ResultCode } from "@/common/enums"
+
+// export type FieldError = {
+//   error: string
+//   field: string
+// }
+
+export const fieldErrorSchema = z.object({
+  error: z.string(),
+  field: z.string(),
+})
+
+type FieldError = z.infer<typeof fieldErrorSchema>
 
 export type BaseResponse<T = {}> = {
   data: T
@@ -9,5 +19,15 @@ export type BaseResponse<T = {}> = {
   messages: string[]
   fieldsErrors: FieldError[]
 }
+
+export const baseResponseSchema = <T extends z.ZodTypeAny>(schema: T) => z.object({
+  data: schema,
+  resultCode: z.nativeEnum(ResultCode),
+  messages: z.string().array(),
+  fieldsErrors: fieldErrorSchema.array(),
+})
+
+export const defaultResponseSchema = baseResponseSchema(z.object({}))
+export type DefaultResponse = z.infer<typeof defaultResponseSchema>
 
 export type RequestStatus = "idle" | "loading" | "succeeded" | "failed"
